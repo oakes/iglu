9c7612654ec9c6645e0450f122434abd4cfe7224
oakes <zach@sekao.net>
2019-02-02 18:08:26 -0600

    Load image

--- src/iglu/examples.cljs
+++ src/iglu/examples.cljs
@@ -16,48 +16,6 @@
       (set! canvas.width display-width)
       (set! canvas.height display-height))))
 
-(def vertex-shader-source
-  "#version 300 es
-  
-  // an attribute is an input (in) to a vertex shader.
-  // It will receive data from a buffer
-  in vec2 a_position;
-  
-  uniform vec2 u_resolution;
-  
-  // all shaders have a main function
-  void main() {
-    // convert the position from pixels to 0.0 to 1.0
-    vec2 zeroToOne = a_position / u_resolution;
- 
-    // convert from 0->1 to 0->2
-    vec2 zeroToTwo = zeroToOne * 2.0;
- 
-    // convert from 0->2 to -1->+1 (clipspace)
-    vec2 clipSpace = zeroToTwo - 1.0;
-  
-    // gl_Position is a special variable a vertex shader
-    // is responsible for setting
-    gl_Position = vec4(clipSpace, 0, 1);
-  }")
-
-(def fragment-shader-source
-  "#version 300 es
-  
-  // fragment shaders don't have a default precision so we need
-  // to pick one. mediump is a good default. It means 'medium precision'
-  precision mediump float;
-  
-  uniform vec4 u_color;
-  
-  // we need to declare an output for the fragment shader
-  out vec4 outColor;
-  
-  void main() {
-    // Just set the output to a constant redish-purple
-    outColor = u_color;
-  }")
-
 (defn create-shader [gl type source]
   (let [shader (.createShader gl type)]
     (.shaderSource gl shader source)
@@ -87,33 +45,74 @@
       (js/Float32Array. (array x1 y1, x2 y1, x1 y2, x1 y2, x2 y1, x2 y2))
       gl.STATIC_DRAW)))
 
-(defn run-fundamentals [canvas]
+(def fundamentals-vertex-shader-source
+  "#version 300 es
+  
+  // an attribute is an input (in) to a vertex shader.
+  // It will receive data from a buffer
+  in vec2 a_position;
+  
+  uniform vec2 u_resolution;
+  
+  // all shaders have a main function
+  void main() {
+    // convert the position from pixels to 0.0 to 1.0
+    vec2 zeroToOne = a_position / u_resolution;
+ 
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+ 
+    // convert from 0->2 to -1->+1 (clipspace)
+    vec2 clipSpace = zeroToTwo - 1.0;
+  
+    // gl_Position is a special variable a vertex shader
+    // is responsible for setting
+    gl_Position = vec4(clipSpace, 0, 1);
+  }")
+
+(def fundamentals-fragment-shader-source
+  "#version 300 es
+  
+  // fragment shaders don't have a default precision so we need
+  // to pick one. mediump is a good default. It means 'medium precision'
+  precision mediump float;
+  
+  uniform vec4 u_color;
+  
+  // we need to declare an output for the fragment shader
+  out vec4 outColor;
+  
+  void main() {
+    // Just set the output to a constant redish-purple
+    outColor = u_color;
+  }")
+
+(defn fundamentals-init [canvas]
   (let [gl (.getContext canvas "webgl2")
-        program (create-program gl vertex-shader-source fragment-shader-source)
-        pos-attrib-location (.getAttribLocation gl program "a_position")
-        pos-buffer (.createBuffer gl)
-        _ (.bindBuffer gl gl.ARRAY_BUFFER pos-buffer)
-        _ (.bufferData gl gl.ARRAY_BUFFER
-            (js/Float32Array. (array 0 0, 0 0.5, 0.7 0))
-            gl.STATIC_DRAW)
-        vao (.createVertexArray gl)
-        _ (.bindVertexArray gl vao)
-        _ (.enableVertexAttribArray gl pos-attrib-location)
-        size 2, type gl.FLOAT, normalize false, stride 0, offset 0
-        _ (.vertexAttribPointer gl pos-attrib-location size type normalize stride offset)
-        resolution-uniform-location (.getUniformLocation gl program "u_resolution")
-        _ (.bufferData gl gl.ARRAY_BUFFER
-            (js/Float32Array. (array 10 20, 80 20, 10 30, 10 30, 80 20, 80 30))
-            gl.STATIC_DRAW)
+        program (create-program gl
+                  fundamentals-vertex-shader-source
+                  fundamentals-fragment-shader-source)
+        vao (let [vao (.createVertexArray gl)]
+              (.bindVertexArray gl vao)
+              vao)
+        pos-buffer (let [pos-attrib-location (.getAttribLocation gl program "a_position")
+                         pos-buffer (.createBuffer gl)
+                         _ (.bindBuffer gl gl.ARRAY_BUFFER pos-buffer)
+                         _ (.enableVertexAttribArray gl pos-attrib-location)
+                         size 2, type gl.FLOAT, normalize false, stride 0, offset 0
+                         _ (.vertexAttribPointer gl pos-attrib-location size type normalize stride offset)]
+                     pos-buffer)
+        resolution-location (.getUniformLocation gl program "u_resolution")
         color-location (.getUniformLocation gl program "u_color")]
     (resize-canvas canvas)
     (.viewport gl 0 0 gl.canvas.width gl.canvas.height)
     (.clearColor gl 0 0 0 0)
-    (.clear gl gl.COLOR_BUFFER_BIT)
+    (.clear gl (bit-or gl.COLOR_BUFFER_BIT gl.DEPTH_BUFFER_BIT))
     (.useProgram gl program)
-    (.uniform2f gl resolution-uniform-location gl.canvas.width gl.canvas.height)
     (.bindVertexArray gl vao)
+    (.uniform2f gl resolution-location gl.canvas.width gl.canvas.height)
     (dotimes [_ 50]
+      (.bindBuffer gl gl.ARRAY_BUFFER pos-buffer)
       (set-rectangle gl (rand-int 300) (rand-int 300) (rand-int 300) (rand-int 300))
       (.uniform4f gl color-location (rand) (rand) (rand) 1)
       (.drawArrays gl gl.TRIANGLES 0 6))))
@@ -121,5 +120,121 @@
 (defexample iglu.core/fundamentals
   {:with-card card}
   (defonce fundamentals-canvas (iglu.examples/create-canvas card))
-  (iglu.examples/run-fundamentals fundamentals-canvas))
+  (iglu.examples/fundamentals-init fundamentals-canvas))
+
+(def image-vertex-shader-source
+  "#version 300 es
+  
+  // an attribute is an input (in) to a vertex shader.
+  // It will receive data from a buffer
+  in vec2 a_position;
+  
+  in vec2 a_texCoord;
+  
+  uniform vec2 u_resolution;
+  
+  out vec2 v_texCoord;
+  
+  // all shaders have a main function
+  void main() {
+    // convert the position from pixels to 0.0 to 1.0
+    vec2 zeroToOne = a_position / u_resolution;
+ 
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+ 
+    // convert from 0->2 to -1->+1 (clipspace)
+    vec2 clipSpace = zeroToTwo - 1.0;
+  
+    // gl_Position is a special variable a vertex shader
+    // is responsible for setting
+    gl_Position = vec4(clipSpace, 0, 1);
+  
+    // pass the texCoord to the fragment shader
+    // The GPU will interpolate this value between points
+    v_texCoord = a_texCoord;
+  }")
+
+(def image-fragment-shader-source
+  "#version 300 es
+  
+  precision mediump float;
+   
+  // our texture
+  uniform sampler2D u_image;
+   
+  // the texCoords passed in from the vertex shader.
+  in vec2 v_texCoord;
+   
+  // we need to declare an output for the fragment shader
+  out vec4 outColor;
+   
+  void main() {
+     // Look up a color from the texture.
+     outColor = texture(u_image, v_texCoord).bgra;
+  }")
+
+(defn image-render [canvas image]
+  (let [gl (.getContext canvas "webgl2")
+        program (create-program gl
+                  image-vertex-shader-source
+                  image-fragment-shader-source)
+        vao (let [vao (.createVertexArray gl)]
+              (.bindVertexArray gl vao)
+              vao)
+        pos-buffer (let [pos-attrib-location (.getAttribLocation gl program "a_position")
+                         pos-buffer (.createBuffer gl)
+                         _ (.bindBuffer gl gl.ARRAY_BUFFER pos-buffer)
+                         _ (.enableVertexAttribArray gl pos-attrib-location)
+                         size 2, type gl.FLOAT, normalize false, stride 0, offset 0
+                         _ (.vertexAttribPointer gl pos-attrib-location size type normalize stride offset)]
+                     pos-buffer)
+        tex-coord-buffer (let [tex-coord-attrib-location (.getAttribLocation gl program "a_texCoord")
+                               tex-coord-buffer (.createBuffer gl)
+                               _ (.bindBuffer gl gl.ARRAY_BUFFER tex-coord-buffer)
+                               _ (.bufferData gl gl.ARRAY_BUFFER
+                                   (js/Float32Array. (array
+                                                       0.0 0.0, 1.0 0.0, 0.0 1.0
+                                                       0.0 1.0, 1.0 0.0, 1.0 1.0))
+                                   gl.STATIC_DRAW)
+                               _ (.enableVertexAttribArray gl tex-coord-attrib-location)
+                               size 2, type gl.FLOAT, normalize false, stride 0, offset 0
+                               _ (.vertexAttribPointer gl tex-coord-attrib-location
+                                   size type normalize stride offset)]
+                           tex-coord-buffer)
+        resolution-location (.getUniformLocation gl program "u_resolution")
+        image-location (.getUniformLocation gl program "u_image")
+        texture-unit 0]
+    (let [texture (.createTexture gl)]
+      (.activeTexture gl (+ gl.TEXTURE0 texture-unit))
+      (.bindTexture gl gl.TEXTURE_2D texture)
+      (.texParameteri gl gl.TEXTURE_2D gl.TEXTURE_WRAP_S gl.CLAMP_TO_EDGE)
+      (.texParameteri gl gl.TEXTURE_2D gl.TEXTURE_WRAP_T gl.CLAMP_TO_EDGE)
+      (.texParameteri gl gl.TEXTURE_2D gl.TEXTURE_MIN_FILTER gl.NEAREST)
+      (.texParameteri gl gl.TEXTURE_2D gl.TEXTURE_MAG_FILTER gl.NEAREST))
+    (let [mip-level 0, internal-fmt gl.RGBA, src-fmt gl.RGBA, src-type gl.UNSIGNED_BYTE]
+      (.texImage2D gl gl.TEXTURE_2D mip-level internal-fmt src-fmt src-type image))
+    (resize-canvas canvas)
+    (.viewport gl 0 0 gl.canvas.width gl.canvas.height)
+    (.clearColor gl 0 0 0 0)
+    (.clear gl (bit-or gl.COLOR_BUFFER_BIT gl.DEPTH_BUFFER_BIT))
+    (.useProgram gl program)
+    (.bindVertexArray gl vao)
+    (.uniform2f gl resolution-location gl.canvas.width gl.canvas.height)
+    (.uniform1i gl image-location texture-unit)
+    (.bindBuffer gl gl.ARRAY_BUFFER pos-buffer)
+    (set-rectangle gl 0 0 image.width image.height)
+    (.drawArrays gl gl.TRIANGLES 0 6)))
+
+(defn image-init [canvas]
+  (let [image (js/Image.)]
+    (doto image
+      (-> .-src (set! "leaves.jpg"))
+      (-> .-onload (set! (fn []
+                           (image-render canvas image)))))))
+
+(defexample iglu.core/image
+  {:with-card card}
+  (defonce image-canvas (iglu.examples/create-canvas card))
+  (iglu.examples/image-init image-canvas))
 
