ea307118cc25a3fbd3c8daa679c6eb7d43fe3a43
oakes <zach@sekao.net>
2019-02-24 11:52:27 -0600

    Instrument during dev

--- src/iglu/dev.cljs
+++ src/iglu/dev.cljs
@@ -3,4 +3,10 @@
             iglu.examples-2d
             iglu.examples-3d
             iglu.examples-advanced
-            dynadoc.core))
+            dynadoc.core
+            [orchestra-cljs.spec.test :as st]
+            [expound.alpha :as expound]
+            [clojure.spec.alpha :as s]))
+
+(st/instrument)
+(set! s/*explain-out* expound/printer)
