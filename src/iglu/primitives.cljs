5462532fa897ed4bc14675d89986ae0718be3449
oakes <zach@sekao.net>
2019-02-23 21:25:50 -0600

    Add truncated-cone-3d example

--- src/iglu/primitives.cljs
+++ src/iglu/primitives.cljs
@@ -33,31 +33,34 @@
            :normals (transient [])
            :texcoords (transient [])}
           (range (inc subdivisions-depth)))
+        (assoc :indices
+          (reduce
+            (fn [indices z]
+              (reduce
+                (fn [indices x]
+                  (-> indices
+                      ;; triangle 1
+                      (conj! (-> z (* num-verts-across) (+ x)))
+                      (conj! (-> z inc (* num-verts-across) (+ x)))
+                      (conj! (-> z (* num-verts-across) (+ (inc x))))
+                      ;; triangle 2
+                      (conj! (-> z inc (* num-verts-across) (+ x)))
+                      (conj! (-> z inc (* num-verts-across) (+ (inc x))))
+                      (conj! (-> z (* num-verts-across) (+ (inc x))))))
+                indices
+                (range subdivisions-width)))
+            (transient [])
+            (range subdivisions-depth)))
         (update :positions persistent!)
         (update :normals persistent!)
         (update :texcoords persistent!)
-        (assoc :indices
-          (persistent!
-            (reduce
-              (fn [indices z]
-                (reduce
-                  (fn [indices x]
-                    (-> indices
-                        ;; triangle 1
-                        (conj! (-> z (* num-verts-across) (+ x)))
-                        (conj! (-> z inc (* num-verts-across) (+ x)))
-                        (conj! (-> z (* num-verts-across) (+ (inc x))))
-                        ;; triangle 2
-                        (conj! (-> z inc (* num-verts-across) (+ x)))
-                        (conj! (-> z inc (* num-verts-across) (+ (inc x))))
-                        (conj! (-> z (* num-verts-across) (+ (inc x))))))
-                  indices
-                  (range subdivisions-width)))
-              (transient [])
-              (range subdivisions-depth)))))))
+        (update :indices persistent!))))
 
+(s/def ::radius number?)
+(s/def ::subdivisions-axis pos?)
+(s/def ::subdivisions-height pos?)
 (s/fdef sphere
-  :args (s/cat :radius number? :axis pos? :height pos?))
+  :args (s/cat :props (s/keys :req-un [::radius ::subdivisions-axis ::subdivisions-height])))
 
 (defn sphere [{:keys [radius subdivisions-axis subdivisions-height]}]
   (let [start-latitude 0
@@ -104,28 +107,28 @@
            :normals (transient [])
            :texcoords (transient [])}
           (range (inc subdivisions-height)))
+        (assoc :indices
+          (reduce
+            (fn [indices y]
+              (reduce
+                (fn [indices x]
+                  (-> indices
+                      ;; triangle 1
+                      (conj! (-> y (* num-verts-around) (+ x)))
+                      (conj! (-> y (* num-verts-around) (+ (inc x))))
+                      (conj! (-> y inc (* num-verts-around) (+ x)))
+                      ;; triangle 2
+                      (conj! (-> y inc (* num-verts-around) (+ x)))
+                      (conj! (-> y (* num-verts-around) (+ (inc x))))
+                      (conj! (-> y inc (* num-verts-around) (+ (inc x))))))
+                indices
+                (range subdivisions-axis)))
+            (transient [])
+            (range subdivisions-height)))
         (update :positions persistent!)
         (update :normals persistent!)
         (update :texcoords persistent!)
-        (assoc :indices
-          (persistent!
-            (reduce
-              (fn [indices y]
-                (reduce
-                  (fn [indices x]
-                    (-> indices
-                        ;; triangle 1
-                        (conj! (-> y (* num-verts-around) (+ x)))
-                        (conj! (-> y (* num-verts-around) (+ (inc x))))
-                        (conj! (-> y inc (* num-verts-around) (+ x)))
-                        ;; triangle 2
-                        (conj! (-> y inc (* num-verts-around) (+ x)))
-                        (conj! (-> y (* num-verts-around) (+ (inc x))))
-                        (conj! (-> y inc (* num-verts-around) (+ (inc x))))))
-                  indices
-                  (range subdivisions-axis)))
-              (transient [])
-              (range subdivisions-height)))))))
+        (update :indices persistent!))))
 
 (def cube-face-indices
   [; right
@@ -197,6 +200,116 @@
            :texcoords (transient [])
            :indices (transient [])}
           (range 6))
+        (update :positions persistent!)
+        (update :normals persistent!)
+        (update :texcoords persistent!)
+        (update :indices persistent!))))
+
+(s/def ::bottom-radius number?)
+(s/def ::top-radius number?)
+(s/def ::radial-subdivisions #(>= % 3))
+(s/def ::vertical-subdivisions #(>= % 1))
+(s/def ::top-cap? boolean?)
+(s/def ::bottom-cap? boolean?)
+(s/fdef truncated-cone
+  :args (s/cat :props (s/keys
+                        :req-un [::bottom-radius ::top-radius
+                                 ::radial-subdivisions ::vertical-subdivisions]
+                        :opt-un [::top-cap? ::bottom-cap?])))
+
+(defn truncated-cone [{:keys [bottom-radius top-radius height
+                              radial-subdivisions vertical-subdivisions
+                              top-cap? bottom-cap?]
+                       :or {top-cap? true bottom-cap? true}}]
+  (let [extra (+ (if top-cap? 2 0) (if bottom-cap? 2 0))
+        num-vertices (* (inc radial-subdivisions)
+                        (+ vertical-subdivisions 1 extra))
+        verts-around-edge (inc radial-subdivisions)
+        slant (js/Math.atan2 (- bottom-radius top-radius) height)
+        cos-slant (js/Math.cos slant)
+        sin-slant (js/Math.sin slant)
+        start (if top-cap? -2 0)
+        end (+ vertical-subdivisions (if bottom-cap? 2 0))]
+    (-> (fn [m yy]
+          (let [v (/ yy vertical-subdivisions)
+                y (* height v)
+                [y v ring-radius]
+                (cond
+                  (< yy 0)
+                  [0
+                   1
+                   bottom-radius]
+                  (> yy vertical-subdivisions)
+                  [height
+                   1
+                   top-radius]
+                  :else
+                  [y
+                   v
+                   (+ bottom-radius
+                      (* (- top-radius bottom-radius)
+                         (/ yy vertical-subdivisions)))])
+                [y v ring-radius]
+                (if (or (= yy -2)
+                        (= yy (+ vertical-subdivisions 2)))
+                  [y 0 0]
+                  [y v ring-radius])
+                y (- y (/ height 2))]
+            (reduce
+              (fn [m ii]
+                (let [sin (js/Math.sin (-> ii (* js/Math.PI) (* 2) (/ radial-subdivisions)))
+                      cos (js/Math.cos (-> ii (* js/Math.PI) (* 2) (/ radial-subdivisions)))]
+                  (-> m
+                      (update :positions (fn [positions]
+                                           (-> positions
+                                               (conj! (* sin ring-radius))
+                                               (conj! y)
+                                               (conj! (* cos ring-radius)))))
+                      (update :normals (fn [normals]
+                                         (-> normals
+                                             (conj! (if (or (< yy 0)
+                                                            (> yy vertical-subdivisions))
+                                                      0
+                                                      (* sin cos-slant)))
+                                             (conj! (if (< yy 0)
+                                                      -1
+                                                      (if (> yy vertical-subdivisions)
+                                                        1
+                                                        sin-slant)))
+                                             (conj! (if (or (< yy 0)
+                                                            (> yy vertical-subdivisions))
+                                                      0
+                                                      (* cos cos-slant))))))
+                      (update :texcoords (fn [texcoords]
+                                           (-> texcoords
+                                               (conj! (/ ii radial-subdivisions))
+                                               (conj! (- 1 v))))))))
+              
+              m
+              (range verts-around-edge))))
+        (reduce
+          {:positions (transient [])
+           :normals (transient [])
+           :texcoords (transient [])}
+          (range start (inc end)))
+        (assoc :indices
+          (reduce
+            (fn [indices yy]
+              (reduce
+                (fn [indices ii]
+                  (-> indices
+                      ;; triangle 1
+                      (conj! (-> verts-around-edge (* (+ yy 0)) (+ 0) (+ ii)))
+                      (conj! (-> verts-around-edge (* (+ yy 0)) (+ 1) (+ ii)))
+                      (conj! (-> verts-around-edge (* (+ yy 1)) (+ 1) (+ ii)))
+                      ;; triangle 2
+                      (conj! (-> verts-around-edge (* (+ yy 0)) (+ 0) (+ ii)))
+                      (conj! (-> verts-around-edge (* (+ yy 1)) (+ 1) (+ ii)))
+                      (conj! (-> verts-around-edge (* (+ yy 1)) (+ 0) (+ ii)))))
+                indices
+                (range radial-subdivisions)))
+            (transient [])
+            (range (+ vertical-subdivisions extra))))
         (update :positions persistent!)
         (update :normals persistent!)
         (update :texcoords persistent!)
