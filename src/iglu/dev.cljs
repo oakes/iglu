11371877767c7cfab6f454fe651d34c6316dd629
oakes <zach@sekao.net>
2019-02-24 16:04:14 -0600

    Move webgl stuff out of the library

--- src/iglu/dev.cljs
+++ src/iglu/dev.cljs
@@ -1,8 +1,5 @@
 (ns iglu.dev
   (:require iglu.examples
-            iglu.examples-2d
-            iglu.examples-3d
-            iglu.examples-advanced
             dynadoc.core
             [orchestra-cljs.spec.test :as st]
             [expound.alpha :as expound]
