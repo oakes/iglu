808058ee65b791d2bbb75cc60facf7527c3a4e41
oakes <zach@sekao.net>
2019-07-10 19:16:35 -0400

    Start 0.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject iglu "0.5.0"
+(defproject iglu "0.6.0-SNAPSHOT"
   :description "A Clojure data -> GLSL library"
   :url "https://github.com/oakes/iglu"
   :license {:name "Public Domain"
