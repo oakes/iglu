6c142c05d1704d944699e30abfbd0c2a9c8fc809
oakes <zach@sekao.net>
2019-07-22 13:27:23 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject iglu "0.6.0-SNAPSHOT"
+(defproject iglu "0.6.0"
   :description "A Clojure data -> GLSL library"
   :url "https://github.com/oakes/iglu"
   :license {:name "Public Domain"
